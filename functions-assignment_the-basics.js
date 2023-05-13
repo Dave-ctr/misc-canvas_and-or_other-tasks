e7a957f988bace18f41d2e709547bfbfbeb4ff31
Dave <davejtake2@gmail.com>
2023-05-14 02:38:47 +1200

    change

--- functions-assignment_the-basics.js
+++ functions-assignment_the-basics.js
@@ -1,3 +1,12 @@
-function tocodeornottocode {
-    
+function tocodeornottocode(time) {
+    if (time < 2300)
+    return "keep coding" 
+ else 
+    return "go to bed"
 }
+
+tocodeornottocode(2230);
+
+
+
+
