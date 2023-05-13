ffd21a1ee7b979008072aca04583f15f4be9a373
Dave <davejtake2@gmail.com>
2023-05-14 10:11:31 +1200

    chur

--- function-assignment.js
+++ function-assignment.js
@@ -1,19 +1,37 @@
-/*function tocodeornottocode(time) {
-  if (time < 2300) {
+// A named function:
+
+/*function toCodeOrNotToCode(time) {
+  if ((time >= 0900) && (time <= 2300)) {
     return "keep coding";
   } else {
     return "go to bed";
   }
-}*/
+}
 
-function tocodeornottocode(time) {
-  const hour = Number(time.toString().slice(0, 2)); // Extract the first two digits of the time and convert to a number
-  
-  if (hour >= 22 || hour < 9) { // If the hour is between 10:00 PM and 9:00 AM
-    return "go to bed";
-  } else { // If the hour is between 9:00 AM and 10:00 PM
+console.log(toCodeOrNotToCode(2315));*/
+
+
+// An anonymous function assigned to a variable:
+
+/*const toCodeOrNotToCode = function(time) {
+  if ((time >= 0900) && (time <= 2300)) {
     return "keep coding";
+  } else {
+    return "go to bed";
   }
 }
 
-console.log(tocodeornottocode(2245));
+console.log(toCodeOrNotToCode(1015));*/
+
+
+// An anonymous fat arrow function assigned to a variable:
+
+const toCodeOrNotToCode = (time) => {
+  if ((time >= 0900) && (time <= 2300)) {
+    return "keep coding";
+  } else {
+    return "go to bed";
+  }
+}
+
+console.log(toCodeOrNotToCode(2300));
