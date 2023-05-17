b685ca94b975057d279fc701828de278a66df4cd
Dave-ctr <83482840+Dave-ctr@users.noreply.github.com>
2023-05-17 12:21:50 +1200

    sorted

---
+++ If-Else-Statement-Challenge/hotdog.js
@@ -0,0 +1,8 @@
+function saleHotdogs(n) {
+  if (n < 5) return (centTotal = 100);
+ else if (n >= 5 && n < 10) return (centTotal = 95);
+  else n >= 10;
+  return (centTotal = 90);
+}
+
+console.log(saleHotdog(22));
