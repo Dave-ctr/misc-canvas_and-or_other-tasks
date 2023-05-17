0e7d63a9e262ba07274a6d443d0dc419de4b01a0
Your Name~ <davidjamieson1985@gmail.com>
2023-05-18 09:03:14 +1200

    complete

--- for-loops/for-loops.js
+++ for-loops/for-loops.js
@@ -1,19 +1,18 @@
-// Life without loops
+// Life without loops is unconditionally tedious
 
+/*
 console.log(0);
 console.log(1);
 console.log(2);
 console.log(3);
-//...
 
 console.log("====================");
-
-// 
-
+ 
 for (let i=0; i<=15; i++) {
     console.log(i);
-}
+}*/
 
+// Loops must have:
 // Initialiser or Starter or starting point
 // Condition for when your loop should end
 // Incrementor or decrementor || continues up or down
@@ -22,6 +21,7 @@ for (let i=0; i<=15; i++) {
 //   console.log(i);
 //}
 
+/*
 i += 1;
 
 i = i + 1; // i = 0 + 1 // i = 1
@@ -30,5 +30,35 @@ i = i + 1; // i = 2 + 1 // 1 = 3
 i = i + 1; // i = 3 + 1 // 1 = 4
 i = i + 1; // i = 4 + 1 // 1 = 5
 
- 
+for (i=0; i<=99; i+=3) {
+    console.log(i);
+}
 
+let total = 0;
+for (let num = 1; num < 100; num++) {
+    console.log(num);
+    total += num;
+}
+
+console.log("The total is: " + total);
+
+let fruits = ["mango", "banana", "apple", "cherry", "orange", "tomato"];
+
+for (let i=0; i<fruits.length; i++) {
+    console.log(fruits[i]);
+}*/
+
+let data = {
+    name: "john",
+    age: 28,
+    maritalStatus: true
+}
+
+// elem = data, name, age / data[elem] = elem: ... / elems are strings
+for (let elem in data) {
+    console.log(elem, data[elem]);
+}
+
+console.log(data);
+console.log("I am anything: ", data.age);
+// refer map, filter etc to get specific returns
